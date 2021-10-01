7ada98928ec989cc6ba0769c095196a24601c4cc
JudyMotha <judyxaviermotha@gmail.com>
2021-10-01 07:34:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 //Refer Crowdfunding Mini project July2021
-//helpers handlebars sessions
+//Server code refrces routes ,db,port,;calls  helpers handlebars sessions template engines
+
+
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({helpers});
-
 const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-
 const sess = {
   secret: "Super secret secret",
   cookie: {},
@@ -32,14 +30,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
 app.use(routes);
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('PORT Listening'));
 });
